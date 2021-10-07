56b08472f1d333ecead139d27ecfe1f9635a17f4
m.witti <https://svn2.se.jku.at/scm/git/pk-ss20-ploesch-team-03>
2021-10-07 13:21:29 +0200

    eulerfive

---
+++ Euler1-10/EulerFive.java
@@ -0,0 +1,7 @@
+public class EulerFive {
+    public static void main(String[] args) {
+        EulerLib eulerLib = new EulerLib();
+        System.out.println(eulerLib.smallestEvenDivisible());
+    }
+    
+}
--- libs/EulerLib.java
+++ libs/EulerLib.java
@@ -83,6 +83,15 @@ public class EulerLib {
     }
 
 
+    public int smallestEvenDivisible() {
+        int i = 1;
+        while(true) {
+            if(i % 11 == 0 && i % 15 == 0 && i % 13 == 0 && i % 14 == 0 && i % 16 == 0 && i % 17 == 0 && i % 18 == 0 && i % 19 == 0 && i % 20 == 0) 
+                return i;           
+            i++;
+        }
+    }
+
 
 
 }
